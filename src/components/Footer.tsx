bad07d384ee55c76407049ce331b80215467105d
NY64 <me@ny64.kr>
2024-11-15 09:34:39 +0900

    chore: 푸터에 달 아이콘 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
 	return (
 		<footer className="mx-auto mt-24 w-full">
-			<div className="border-t border-neutral-800 pt-5">
+			<div className="border-t border-neutral-800 pt-5 flex justify-between items-center">
 				<p className="text-neutral-300 text-sm">© 2024 NY64. All rights reserved.</p>
+				<p className="grayscale">🌙</p>
 			</div>
 		</footer>
 	);
