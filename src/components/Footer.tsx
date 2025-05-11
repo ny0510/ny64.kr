1f17c0e5ac12d59865a007a2590d379f92aa8557
ny64 <me@ny64.kr>
2025-05-12 00:25:25 +0900

    오탈자 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="text-sm text-muted-foreground">
-      <p>&copy; 2025 ny64 All rights reserved.</p>
+      <p>&copy; 2025 ny64. All rights reserved.</p>
     </footer>
   );
 }
