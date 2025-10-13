a7256bd068fd89c0b7024bcaa75dc4fc4f4c1275
ny64 <me@ny64.kr>
2025-10-13 09:01:49 +0900

    chore: 수상 및 경험 목록 수정

--- src/components/AchievementsSection.tsx
+++ src/components/AchievementsSection.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { SquareArrowOutUpRight } from 'lucide-react';
 
 const achievements = [
-  { date: '2025-09-21', title: "Daangn Builder's Camp 2025 해커톤 우승 (1위)", tags: ['Award'] },
-  { date: '2024-11-01', title: '제 20회 국제특허정보박람회 부스 운영', tags: ['Conference'], url: 'https://patinex.org' },
-  { date: '2024-10-29', title: '한국코드페어 SW 공모전 동상 수상', tags: ['Award'], url: 'https://www.kcf.or.kr/notice/?q=YToxOntzOjEyOiJrZXl3b3JkX3R5cGUiO3M6MzoiYWxsIjt9&bmode=view&idx=123951920' },
-  { date: '2024-10-04', title: '리눅스마스터 2급 자격증 취득', tags: ['Certification'] },
-  { date: '2024-09-20', title: 'Gencon 2024 AI Conference for DEV 참여', tags: ['Conference'], url: 'https://gencon2024.fastcampus.co.kr' },
+  { date: '2025-09-21', title: "제 1회 Daangn Builder's Camp 해커톤 (1위)", tags: ['Award'] },
+  { date: '2025-09-21', title: "제 1회 Daangn Builder's Camp 수료", tags: ['Education'] },
+  { date: '2024-11-01', title: '제 20회 국제특허정보박람회 (PATINEX) 부스 운영', tags: ['Conference'], url: 'https://patinex.org' },
+  { date: '2024-10-29', title: '한국코드페어 SW 공모전 동상 (3위)', tags: ['Award'], url: 'https://www.kcf.or.kr/notice/?q=YToxOntzOjEyOiJrZXl3b3JkX3R5cGUiO3M6MzoiYWxsIjt9&bmode=view&idx=123951920' },
+  { date: '2024-10-04', title: '리눅스마스터 2급 취득', tags: ['Certification'] },
+  // { date: '2024-09-20', title: 'Gencon 2024 AI Conference for DEV 참여', tags: ['Conference'], url: 'https://gencon2024.fastcampus.co.kr' },
   { date: '2024-08-24', title: '제11회 대한민국 SW융합 해커톤 본선 진출', tags: ['Hackathon'], url: 'https://www.swhackathon.kr' },
   { date: '2024-08-09', title: 'JUNCTION ASIA 2024 해커톤 본선 진출', tags: ['Hackathon'], url: 'https://eu.junctionplatform.com/projects/junction-asia-2024' },
   { date: '2023-11-09', title: '콘텐츠유니버스 코리아 테크 해커톤 본선 진출', tags: ['Hackathon'], url: 'https://contentsuniverse.com/kr/2023contest' },
-  { date: '2023-09-09', title: 'UbuCon Korea 2023 참여', tags: ['Conference'], url: 'https://2023.ubuntu-kr.org/ko' },
+  // { date: '2023-09-09', title: 'UbuCon Korea 2023 참여', tags: ['Conference'], url: 'https://2023.ubuntu-kr.org/ko' },
   { date: '2023-03-02', title: '선린인터넷고등학교 118기 소프트웨어과 합격', tags: ['Education'] },
 ];
 
