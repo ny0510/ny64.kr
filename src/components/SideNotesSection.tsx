3823bba52f7adbd08b5f7b2566c0d7b7776ef89a
ny64 <me@ny64.kr>
2025-12-18 00:52:09 +0900

    chore: 홈서버 사양 수정

--- src/components/SideNotesSection.tsx
+++ src/components/SideNotesSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const sideNotes = [
-  '작은 홈서버를 운영하고 있습니다. (5500GT, 96GB, 22TB Disk)',
+  '작은 홈서버를 운영하고 있습니다. (5500GT, 96GB RAM, 22TB Disk)',
   '개인적으로 JetBrains IDE를 선호하지 않습니다. 램먹는 하마에요. 안드로이드 개발 또한 VSCode를 사용하고 있습니다.',
   'AI/ML 관련 프로젝트가 아니라면 Python을 사용하지 않습니다. 공백만으로 블록을 구분하는 것을 좋아하지 않습니다.',
 ];
